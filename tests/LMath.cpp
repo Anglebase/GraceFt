1ca657fb1155f71ef0f286d959b70175e6d9779a
Anglebase <1628818324@qq.com>
2024-09-04 22:52:28 +0800

    LMath 测试

---
+++ tests/LMath.cpp
@@ -0,0 +1,15 @@
+#include <LMath.hpp>
+
+using namespace GFt;
+using namespace std;
+
+int main() {
+    fVec3 v1 = makefVec3(1.0f, 2.0f, 3.0f);
+    fVec3 v2 = makefVec3(4.0f, 5.0f, 6.0f);
+    auto a = v1 * v2;
+    auto b = v1 ^ v2;
+    cout << "v1 * v2 = " << a << endl;
+    cout << "v1 ^ v2 = " << b << endl;
+    
+    return 0;
+}
