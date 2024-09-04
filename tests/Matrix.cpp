17cd6ae4da3edc5796630c565c9540b56de0b890
Anglebase <1628818324@qq.com>
2024-09-04 13:46:16 +0800

    更新测试用例

--- tests/Matrix.cpp
+++ tests/Matrix.cpp
@@ -25,6 +25,7 @@ int main() {
     cout << "m2*m2^-1: " << (m2 * m2.inverse()).correct() << endl;
     cout << "m1*m1^-1 == I: " << std::boolalpha << (m1 * m1.inverse() == fMat3x3::I()) << endl;
     cout << "m2*m2^-1 == I: " << std::boolalpha << (m2 * m2.inverse() == fMat4x4::I()) << endl;
+    cout << "m2:" << m2.rows() << "x" << m2.cols() << "=" << m2.count() << " elements" << endl;
     
     return 0;
 }
