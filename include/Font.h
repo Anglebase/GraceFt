f375895acd5f0d6d38955307e71f4a9df80e5795
Anglebase <1628818324@qq.com>
2024-09-07 16:25:59 +0800

    实现Font

---
+++ include/Font.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+namespace GFt {
+
+    /// @addtogroup API接口库
+    /// @{
+    /// @addtogroup 枚举
+    /// @{
+    /// @addtogroup 字体枚举
+    /// @{
+
+    /// @enum FontWeight
+    /// @brief 字体粗细枚举
+    enum class FontWeight {
+        Default = 0,        ///< 默认粗细 (0)
+        Thin = 100,         ///< 细体 (100)
+        Extralight = 200,   ///< 极细体 (200)
+        Ultralight = 200,   ///< 超细体 (200)
+        Light = 300,        ///< 细体 (300)
+        Normal = 400,       ///< 正常粗细 (400)
+        Regular = 400,      ///< 正常粗细 (400)
+        Medium = 500,       ///< 中粗体 (500)
+        Semibold = 600,     ///< 半粗体 (600)
+        Demibold = 600,     ///< 半粗体 (600)
+        Bold = 700,         ///< 粗体 (700)
+        Extrabold = 800,    ///< 极粗体 (800)
+        Ultrabold = 800,    ///< 超粗体 (800)
+        Heavy = 900,        ///< 黑体 (900)
+        Black = 900         ///< 黑体 (900)
+    };
+
+    /// @}
+    /// @}
+
+    /// @addtogroup 图形系统
+    /// @{
+    /// @addtogroup 基础类型
+    /// @{
+        /// @class Font
+        /// @brief 字体类
+        /// @details 字体类用于管理字体相关的属性，包括字体名称、大小、粗细、斜体、下划线、删除线等。
+    /// @}
+    /// @}
+    /// @}
+    class Font {
+        void* font_;
+    public:
+        /// @brief 构造函数
+        /// @param fontFamily 字体名称
+        /// @param size 字体大小，默认16
+        Font(const std::wstring& fontFamily, long size = 16);
+        /// @brief 复制构造
+        Font(const Font& other);
+        /// @brief 移动构造
+        Font(Font&& other);
+        /// @brief 复制赋值
+        Font& operator=(const Font& other);
+        /// @brief 移动赋值
+        Font& operator=(Font&& other);
+        ~Font();
+        /// @brief 设置字体大小
+        void setSize(long size);
+        /// @brief 获取字体大小
+        long size() const;
+        /// @brief 设置字体字重(粗细)
+        void setWeight(FontWeight weight);
+        /// @brief 获取字体字重(粗细)
+        FontWeight weight() const;
+        /// @brief 设置字体是否为斜体
+        void setItalic(bool italic);
+        /// @brief 获取字体是否为斜体
+        bool italic() const;
+        /// @brief 设置字体是否具有下划线
+        void setUnderline(bool underline);
+        /// @brief 获取字体是否具有下划线
+        bool underline() const;
+        /// @brief 设置字体是否具有删除线
+        void setStrikeOut(bool strikeOut);
+        /// @brief 获取字体是否具有删除线
+        bool strikeOut() const;
+        /// @brief 设置字体名称
+        void setFontFamily(const std::wstring& fontFamily);
+        /// @brief 获取字体名称
+        std::wstring fontFamily() const;
+        /// @brief 输出流运算符重载
+        friend std::ostream& operator<<(std::ostream& os, const Font& font);
+    };
+}
---
+++ src/Font.cpp
@@ -0,0 +1,62 @@
+#include "Font.h"
+
+#include <ege.h>
+
+#define FONT(x) (reinterpret_cast<LOGFONT*>(x))
+
+namespace GFt {
+    Font::Font(const std::wstring& fontFamily, long size) {
+        auto* font = new LOGFONT();
+        font_ = reinterpret_cast<void*>(font);
+        wcscpy_s(FONT(font)->lfFaceName, fontFamily.c_str());
+        FONT(font)->lfHeight = size;
+
+    }
+    Font::Font(const Font& other) {
+        auto* font = new LOGFONT();
+        font_ = reinterpret_cast<void*>(font);
+        *FONT(font) = *FONT(other.font_);
+    }
+    /// @note 对象被移动后原对象将失效, 它应该被弃置, 如果通过已被移动的对象访问其成员函数, 将引发空指针解引用(段错误)
+    Font::Font(Font&& other) {
+        font_ = other.font_;
+        other.font_ = nullptr;
+    }
+    Font& Font::operator=(const Font& other) {
+        if (this != &other) {
+            delete FONT(font_);
+            auto* font = new LOGFONT();
+            font_ = reinterpret_cast<void*>(font);
+            *FONT(font) = *FONT(other.font_);
+        }
+        return *this;
+    }
+    /// @note 对象被移动后原对象将失效, 它应该被弃置, 如果通过已被移动的对象访问其成员函数, 将引发空指针解引用(段错误)
+    Font& Font::operator=(Font&& other) {
+        if (this != &other) {
+            delete FONT(font_);
+            font_ = other.font_;
+            other.font_ = nullptr;
+        }
+        return *this;
+    }
+    Font::~Font() {
+        delete FONT(font_);
+    }
+    void Font::setSize(long size) { FONT(font_)->lfHeight = size; }
+    long Font::size() const { return FONT(font_)->lfHeight; }
+    void Font::setWeight(FontWeight weight) { FONT(font_)->lfWeight = static_cast<long>(weight); }
+    FontWeight Font::weight() const { return static_cast<FontWeight>(FONT(font_)->lfWeight); }
+    void Font::setItalic(bool italic) { FONT(font_)->lfItalic = italic; }
+    bool Font::italic() const { return FONT(font_)->lfItalic; }
+    void Font::setUnderline(bool underline) { FONT(font_)->lfUnderline = underline; }
+    bool Font::underline() const { return FONT(font_)->lfUnderline; }
+    void Font::setStrikeOut(bool strikeOut) { FONT(font_)->lfStrikeOut = strikeOut; }
+    bool Font::strikeOut() const { return FONT(font_)->lfStrikeOut; }
+    void Font::setFontFamily(const std::wstring& fontFamily) { wcscpy_s(FONT(font_)->lfFaceName, fontFamily.c_str()); }
+    std::wstring Font::fontFamily() const { return FONT(font_)->lfFaceName; }
+    std::ostream& operator<<(std::ostream& os, const Font& font) {
+        os << "Font{ " << &font << ": " << font.font_ << " }";
+        return os;
+    }
+}
