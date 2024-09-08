dce957dde6acbc05228f7f9053ebd5df17e25a2d
Anglebase <1628818324@qq.com>
2024-09-08 15:11:05 +0800

    实现Bezier<T>

---
+++ include/Bezier.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <vector>
+
+#include <Point.hpp>
+
+namespace GFt {
+    /// @class Bezier
+    /// @brief 贝塞尔曲线
+    /// @tparam T 点的类型
+    template<typename T>
+        requires std::is_arithmetic_v<T>
+    class Bezier {
+        std::vector<Point<T>> points;
+    public:
+        /// @brief 默认构造函数
+        Bezier() = default;
+        /// @brief 构造函数
+        /// @param p1 点1
+        /// @param p2 点2
+        /// @param c1 点1控制点
+        /// @param c2 点2控制点
+        Bezier(const Point<T>& p1, const Point<T>& p2, const Point<T>& c1, const Point<T>& c2) {
+            points.push_back(p1);
+            points.push_back(c1);
+            points.push_back(c2);
+            points.push_back(p2);
+        }
+        /// @brief 添加下一段线
+        /// @param c1 新线起始点的控制点
+        /// @param p  新线的结束点
+        /// @param c2 新线结束点的控制点
+        void addPoint(const Point<T>& c1, const Point<T>& p, const Point<T>& c2) {
+            points.push_back(c1);
+            points.push_back(c2);
+            points.push_back(p);
+        }
+        /// @brief 统计点的数量
+        /// @return 点的数量
+        int count() const { return static_cast<int>(points.size()); }
+
+        /// @brief 类型转换函数
+        /// @tparam U 目标类型
+        /// @param b 要转换的贝塞尔曲线
+        /// @return 转换后的贝塞尔曲线
+        template<typename U>
+            requires std::is_arithmetic_v<U>
+        friend Bezier<U> cast(const Bezier<T>& b) {
+            Bezier<U> result;
+            for (const auto& p : b.points)
+                result.addPoint(cast<U>(p));
+            return result;
+        }
+    };
+
+    using iBezier = Bezier<int>;
+    using fBezier = Bezier<float>;
+}
