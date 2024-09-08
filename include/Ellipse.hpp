ead11f81c221f45192007e7ec442e5f76b4a2b1c
Anglebase <1628818324@qq.com>
2024-09-08 14:52:07 +0800

    更改名称

--- include/Ellipse.hpp
+++ include/Ellipse.hpp
@@ -4,33 +4,33 @@
 #include <Rect.hpp>
 
 namespace GFt {
-    /// @class Elliptic
+    /// @class Ellipse
     /// @brief 椭圆类
     /// @tparam T 数值类型, 要求为算术类型
     template<typename T>
         requires std::is_arithmetic_v<T>
-    class Elliptic {
+    class Ellipse {
         Rect<T> rect_;
     public:
         /// @brief 构造函数
         /// @param rect 椭圆的外接矩形
-        constexpr Elliptic(const Rect<T>& rect) : rect_(rect) { rect.normalize(); }
+        constexpr Ellipse(const Rect<T>& rect) : rect_(rect) { rect.normalize(); }
         /// @brief 构造函数
         /// @param center 椭圆的中心点
         /// @param rx 椭圆的水平半径
         /// @param ry 椭圆的竖直半径
-        constexpr Elliptic(const Point<T>& center, T rx, T ry)
+        constexpr Ellipse(const Point<T>& center, T rx, T ry)
             : rect_(center.x - rx, center.y - ry, 2 * rx, 2 * ry) {
             rect_.normalize();
         }
         /// @brief 判断两个椭圆是否相同
         /// @param other 另一个椭圆
         /// @return 若两个椭圆相同, 则返回 true, 否则返回 false
-        constexpr bool operator==(const Elliptic<T>& other) const { return rect_ == other.rect_; }
+        constexpr bool operator==(const Ellipse<T>& other) const { return rect_ == other.rect_; }
         /// @brief 判断两个椭圆是否不同
         /// @param other 另一个椭圆
         /// @return 若两个椭圆不同, 则返回 true, 否则返回 false
-        constexpr bool operator!=(const Elliptic<T>& other) const { return !(*this == other); }
+        constexpr bool operator!=(const Ellipse<T>& other) const { return !(*this == other); }
 
         /// @brief 椭圆的外接矩形
         /// @return 椭圆的外接矩形
@@ -58,8 +58,8 @@ namespace GFt {
         /// @param os 输出流
         /// @param elliptic 椭圆
         /// @return 输出流 os
-        friend std::ostream& operator<<(std::ostream& os, const Elliptic<T>& elliptic) {
-            os << "Elliptic(" << elliptic.rect_ << ")";
+        friend std::ostream& operator<<(std::ostream& os, const Ellipse<T>& elliptic) {
+            os << "Ellipse(" << elliptic.rect_ << ")";
             return os;
         }
         /// @brief 类型转换函数
@@ -68,8 +68,11 @@ namespace GFt {
         /// @return 目标类型椭圆
         template<typename U>
             requires std::is_arithmetic_v<U>
-        friend Elliptic<U> cast(const Elliptic<T>& elliptic) {
-            return Elliptic<U>(cast<U>(elliptic.rect_));
+        friend Ellipse<U> cast(const Ellipse<T>& elliptic) {
+            return Ellipse<U>(cast<U>(elliptic.rect_));
         }
     };
+
+    using iEllipse = Ellipse<int>;
+    using fEllipse = Ellipse<float>;
 }
