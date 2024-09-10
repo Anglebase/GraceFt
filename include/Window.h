e5d8df36078c6d49f6c5cc43fd5a6ca057e4932b
Anglebase <1628818324@qq.com>
2024-09-10 21:48:13 +0800

    实现Window

---
+++ include/Window.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <Block.h>
+
+namespace GFt {
+    /// @class Window
+    /// @brief 全局窗口对象
+    /// @details 此类用于表示一个全局唯一的窗口对象, 并提供了窗口创建的一系列工厂函数
+    /// @details 同时也提供了一些操作窗口的成员函数
+    class Window final : public Block {
+        static Window* pInstance_;
+
+    private:
+        Window(int width, int height, int flags);
+        Window(const Window& other) = delete;
+        Window& operator=(const Window& other) = delete;
+        Window(Window&& other) = delete;
+        Window& operator=(Window&& other) = delete;
+
+    public:
+        ~Window();
+        /// @brief 显示窗口
+        void show();
+        /// @brief 隐藏窗口
+        void hide();
+        /// @brief 更改窗口的大小
+        /// @param rect 新的窗口大小
+        void resize(const iSize& rect);
+        /// @brief 移动窗口(相对位置)
+        /// @param dpos 相对于当前位置的差值
+        void move(const iPoint& dpos);
+        /// @brief 移动窗口(绝对位置)
+        /// @param pos 新的窗口位置
+        void moveTo(const iPoint& pos);
+        /// @brief 设置窗口标题
+        /// @param title 新的标题
+        void setTitle(const std::wstring& title);
+
+        void stop();
+
+    public:
+        /// @brief 创建普通窗口
+        /// @param rect 窗口大小
+        /// @param hide 是否在创建后隐藏窗口
+        static Window* createWindow(const iRect& rect, bool hide = false);
+        /// @brief 创建全屏窗口
+        /// @param hide 是否在创建后隐藏窗口
+        static Window* createFullScreenWindow(bool hide = false);
+        /// @brief 创建置顶窗口
+        /// @param rect 窗口大小
+        /// @param hide 是否在创建后隐藏窗口
+        static Window* createTopMostWindow(const iRect& rect, bool hide = false);
+        /// @brief 创建无边框窗口
+        /// @param rect 窗口大小
+        /// @param hide 是否在创建后隐藏窗口
+        static Window* createNoBorderWindow(const iRect& rect, bool hide = false);
+        /// @brief 创建无边框置顶窗口
+        /// @param rect 窗口大小
+        /// @param hide 是否在创建后隐藏窗口
+        static Window* createNoBorderTopMostWindow(const iRect& rect, bool hide = false);
+        /// @brief 获取已创建的窗口对象实例
+        /// @return 已创建的窗口对象指针
+        static Window* window();
+    };
+}
---
+++ src/Window.cpp
@@ -0,0 +1,82 @@
+#include "Window.h"
+
+#include <Tools.h>
+#include <ege.h>
+
+#define H(hide) (hide? ege::INIT_HIDE : 0)
+
+namespace GFt {
+    using namespace ege;
+    Window* Window::pInstance_ = nullptr;
+    Window::Window(int width, int height, int flags)
+        : Block(iRect(0, 0, width, height)) {
+        initgraph(width, height, flags | INIT_UNICODE | INIT_ANIMATION | INIT_NOFORCEEXIT);
+        setcaption(L"GraceFt v2.0.0");
+        setbkcolor(WHITE);
+        setcolor(BLACK);
+        setfillcolor(EGERGBA(195, 198, 220, 230));
+        cleardevice();
+    }
+    Window::~Window() {
+        closegraph();
+    }
+    void Window::show() { showwindow(); }
+    void Window::hide() { hidewindow(); }
+    void Window::resize(const iSize& rect) { resizewindow(rect.width(), rect.height()); }
+    void Window::move(const iPoint& dpos) {
+        RECT rect;
+        GetWindowRect(getHWnd(), &rect);
+        movewindow(dpos.x() + rect.left, dpos.y() + rect.top);
+    }
+    void Window::moveTo(const iPoint& pos) { movewindow(pos.x(), pos.y()); }
+    void Window::setTitle(const std::wstring& title) { setcaption(title.c_str()); }
+    void Window::stop() { getch(); }
+    Window* Window::createWindow(const iRect& rect, bool hide) {
+        if (Window::pInstance_)
+            return Window::pInstance_;
+        static Window window(rect.width(), rect.height(), H(hide));
+        movewindow(rect.x(), rect.y());
+        flushwindow();
+        Window::pInstance_ = &window;
+        return Window::pInstance_;
+    }
+    Window* Window::createFullScreenWindow(bool hide) {
+        if (Window::pInstance_)
+            return Window::pInstance_;
+        static Window window(100_sw, 100_sh, H(hide) | INIT_NOBORDER | INIT_TOPMOST);
+        movewindow(0, 0);
+        flushwindow();
+        Window::pInstance_ = &window;
+        return Window::pInstance_;
+    }
+    Window* Window::createTopMostWindow(const iRect& rect, bool hide) {
+        if (Window::pInstance_)
+            return Window::pInstance_;
+        static Window window(rect.width(), rect.height(), H(hide) | INIT_TOPMOST);
+        movewindow(rect.x(), rect.y());
+        flushwindow();
+        Window::pInstance_ = &window;
+        return Window::pInstance_;
+    }
+    Window* Window::createNoBorderWindow(const iRect& rect, bool hide) {
+        if (Window::pInstance_)
+            return Window::pInstance_;
+        static Window window(rect.width(), rect.height(), H(hide) | INIT_NOBORDER);
+        movewindow(rect.x(), rect.y());
+        flushwindow();
+        Window::pInstance_ = &window;
+        return Window::pInstance_;
+    }
+    Window* Window::createNoBorderTopMostWindow(const iRect& rect, bool hide) {
+        if (Window::pInstance_)
+            return Window::pInstance_;
+        static Window window(rect.width(), rect.height(), H(hide) | INIT_NOBORDER | INIT_TOPMOST);
+        movewindow(rect.x(), rect.y());
+        flushwindow();
+        Window::pInstance_ = &window;
+        return Window::pInstance_;
+    }
+    Window* Window::window() {
+        return Window::pInstance_;
+    }
+}
