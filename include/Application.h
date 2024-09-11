20378f3a1bdeb012f448a4ac93e39a3a5d2038da
Anglebase <1628818324@qq.com>
2024-09-11 13:26:30 +0800

    移除异步事件

--- include/Application.h
+++ include/Application.h
@@ -3,15 +3,12 @@
 #include <Window.h>
 
 namespace GFt {
-    /// @brief 程序执行模式
-    enum class ExecMode {
-        Sync,   ///< 同步模式: 所有逻辑都在主线程中执行
-        Async   ///< 异步模式: 绘制逻辑和事件处理逻辑分属于两个不同于主线程的线程
-    };
 
     class Application {
         int sync_exec(Window* window);
         int async_exec(Window* window);
+        void render(Window* window);
+        void handleEvents(Window* window);
         static Window* root_;
         static double FPS_;
     private:
@@ -23,7 +20,7 @@ namespace GFt {
     public:
         explicit Application(Window* root);
         ~Application();
-        int exec(ExecMode mode = ExecMode::Sync);
+        int exec();
         double& fps();
         const double& fps() const;
         float getRealFps() const;
--- src/Application.cpp
+++ src/Application.cpp
@@ -1,6 +1,7 @@
 #include "Application.h"
 
 #include <thread>
+#include <mutex>
 #include <ege.h>
 
 namespace GFt {
@@ -13,93 +14,100 @@ namespace GFt {
             return;
         Application::root_ = root;
     }
-
-    int Application::sync_exec(Window* window) {
-        constexpr int max_msg_count = 50;
-        for (;is_run(); Application::FPS_ > 0 ? delay_fps(Application::FPS_) : (void)0) {
-            int count_mousemsg = 0, count_kbmsg = 0, count_textmsg = 0;
-            if (mousemsg()) do {
-                auto msg = getmouse();
-                // 获取鼠标按键
-                MouseButton button;
-                if (msg.is_left())
-                    button = MouseButton::Left;
-                else if (msg.is_right())
-                    button = MouseButton::Right;
-                else if (msg.is_mid())
-                    button = MouseButton::Middle;
-                else
-                    button = MouseButton::Unknown;
-                // 处理鼠标事件
-                if (msg.is_move())
-                    window->handleOnMouseMove(MouseMoveEvent(iPoint{ msg.x, msg.y }));
-                else if (msg.is_down()) {
-                    window->handleOnMouseButtonPress(MouseButtonPressEvent{
-                        iPoint{ msg.x, msg.y }, button,
-                        });
-                }
-                else if (msg.is_up()) {
-                    window->handleOnMouseButtonRelease(MouseButtonReleaseEvent{
-                        iPoint{ msg.x, msg.y }, button,
-                        });
-                }
-                else if (msg.is_wheel())
-                    window->handleOnMouseWheel(MouseWheelEvent{
-                        iPoint{ msg.x, msg.y },
-                        msg.wheel > 0
-                            ? MouseWheel::Up
-                            : msg.wheel < 0
-                                ? MouseWheel::Down
-                                : MouseWheel::None,
-                        });
-                count_mousemsg++;
-                if (count_mousemsg > max_msg_count)
-                    break;
-            } while (mousemsg());
-            if (kbmsg()) do {
-                auto msg = getkey();
-                if (msg.msg == key_msg_down)
-                    window->handleOnKeyPress(KeyPressEvent{
-                        static_cast<Key>(msg.key),
-                        static_cast<bool>(msg.flags & key_flag_shift),
-                        static_cast<bool>(msg.flags & key_flag_ctrl),
-                        });
-                else if (msg.msg == key_msg_up)
-                    window->handleOnKeyRelease(KeyReleaseEvent{
-                        static_cast<Key>(msg.key),
-                        static_cast<bool>(msg.flags & key_flag_shift),
-                        static_cast<bool>(msg.flags & key_flag_ctrl),
-                        });
-                count_kbmsg++;
-                if (count_kbmsg > max_msg_count)
-                    break;
-            } while (kbmsg());
-            if (kbhit()) do {
-                auto ch = getch();
-                window->handleOnTextInput(TextInputEvent{ ch });
-                count_textmsg++;
-                if (count_textmsg > max_msg_count)
-                    break;
-            } while (kbhit());
-            cleardevice();
+    std::mutex handleMutex;
+    void Application::render(Window* window) {
+        cleardevice();
+        {
+            std::lock_guard<std::mutex> lock(handleMutex);
             window->handleOnDraw();
-            flushwindow();
         }
-        return 0;
+        flushwindow();
     }
-    int Application::async_exec(Window* window) {
-        return 0;
+    void Application::handleEvents(Window* window) {
+        constexpr int max_msg_count = 20;
+        int count_mousemsg = 0, count_kbmsg = 0, count_textmsg = 0;
+        if (mousemsg()) do {
+            auto msg = getmouse();
+            // 获取鼠标按键
+            MouseButton button;
+            if (msg.is_left())
+                button = MouseButton::Left;
+            else if (msg.is_right())
+                button = MouseButton::Right;
+            else if (msg.is_mid())
+                button = MouseButton::Middle;
+            else
+                button = MouseButton::Unknown;
+            // 处理鼠标事件
+            if (msg.is_move()) {
+                std::lock_guard<std::mutex> lock(handleMutex);
+                window->handleOnMouseMove(MouseMoveEvent(iPoint{ msg.x, msg.y }));
+            }
+            else if (msg.is_down()) {
+                std::lock_guard<std::mutex> lock(handleMutex);
+                window->handleOnMouseButtonPress(MouseButtonPressEvent{
+                    iPoint{ msg.x, msg.y }, button,
+                    });
+            }
+            else if (msg.is_up()) {
+                std::lock_guard<std::mutex> lock(handleMutex);
+                window->handleOnMouseButtonRelease(MouseButtonReleaseEvent{
+                    iPoint{ msg.x, msg.y }, button,
+                    });
+            }
+            else if (msg.is_wheel()) {
+                std::lock_guard<std::mutex> lock(handleMutex);
+                window->handleOnMouseWheel(MouseWheelEvent{
+                    iPoint{ msg.x, msg.y },
+                    msg.wheel > 0
+                        ? MouseWheel::Up
+                        : msg.wheel < 0
+                            ? MouseWheel::Down
+                            : MouseWheel::None,
+                    });
+            }
+            count_mousemsg++;
+            if (count_mousemsg > max_msg_count)
+                break;
+        } while (mousemsg());
+        if (kbmsg()) do {
+            auto msg = getkey();
+            if (msg.msg == key_msg_down) {
+                std::lock_guard<std::mutex> lock(handleMutex);
+                window->handleOnKeyPress(KeyPressEvent{
+                    static_cast<Key>(msg.key),
+                    static_cast<bool>(msg.flags & key_flag_shift),
+                    static_cast<bool>(msg.flags & key_flag_ctrl),
+                    });
+            }
+            else if (msg.msg == key_msg_up) {
+                std::lock_guard<std::mutex> lock(handleMutex);
+                window->handleOnKeyRelease(KeyReleaseEvent{
+                    static_cast<Key>(msg.key),
+                    static_cast<bool>(msg.flags & key_flag_shift),
+                    static_cast<bool>(msg.flags & key_flag_ctrl),
+                    });
+            }
+            count_kbmsg++;
+            if (count_kbmsg > max_msg_count)
+                break;
+        } while (kbmsg());
+        if (kbhit()) do {
+            auto ch = getch();
+            {
+                std::lock_guard<std::mutex> lock(handleMutex);
+                window->handleOnTextInput(TextInputEvent{ ch });
+            }
+            count_textmsg++;
+            if (count_textmsg > max_msg_count)
+                break;
+        } while (kbhit());
     }
-
     Application::~Application() {}
-    int Application::exec(ExecMode mode) {
-        switch (mode) {
-        case ExecMode::Sync:
-            return sync_exec(Application::root_);
-        case ExecMode::Async:
-            return async_exec(Application::root_);
-        default:
-            break;
+    int Application::exec() {
+        for (;is_run(); Application::FPS_ > 0 ? delay_fps(Application::FPS_) : (void)0) {
+            handleEvents(Application::root_);
+            render(Application::root_);
         }
         return 0;
     }
