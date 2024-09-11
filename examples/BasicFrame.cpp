53c038c1ff49f3ab686c9a25f8dc8d1a2d2352df
Anglebase <1628818324@qq.com>
2024-09-11 20:36:44 +0800

    添加示例

--- examples/BasicFrame.cpp
+++ examples/BasicFrame.cpp
@@ -1,9 +1,18 @@
 #include <GraceFt/Application.h>
 #include <GraceFt/Window.h>
 #include <GraceFt/Tools.h>
-
-// 这里提供了一个最基本的窗口创建和显示的例子，展示了如何使用GraceFt库。
-
+/**
+ * @example BasicFrame.cpp
+ * 
+ * 这是一个最基本的创建窗口的例子，展示了如何使用GraceFt库创建窗口并显示一个空白的窗口
+ * 通过`using namespace GFt::literals;`你可以引入设备无关的单位字面量，通过它们你
+ * 可以方便地设置设备无关的窗口大小、位置等。
+ *
+ * 在`GFt::Window`中提供了一系列用于创建窗口的工厂函数，它们可以创建多种不同的窗口模式
+ * 以满足不同类型的应用需求。不过，在同一个程序中，你只能创建一个窗口对象实例，当你首次调
+ * 用它们这些工厂函数之一时，就会创建窗口，之后再次调用它们将返回同一个窗口对象实例，而不
+ * 会创建新的窗口。
+ */
 int main() {
     using namespace GFt::literals;                   // 引入设备无关字面量转换命名空间
 
