9a522c45713560f8fb4c12d5def06252f2f612e7
Anglebase <1628818324@qq.com>
2024-09-11 13:26:44 +0800

    更新测试用例

--- tests/Application.cpp
+++ tests/Application.cpp
@@ -1,5 +1,6 @@
 #include <Application.h>
 #include <Graphics.h>
+#include <chrono>
 
 using namespace GFt;
 using namespace std;
@@ -12,6 +13,14 @@ protected:
         g.setBackgroundColor(0xe9e9e9_rgb);
         g.clear();
         g.drawFillRect(fRect{ 20,20,100,100 });
+        auto time =
+            chrono::duration_cast<chrono::milliseconds>(
+                chrono::system_clock::now().time_since_epoch()
+            ).count() / 1000.0;
+        g.drawLine(fLine{
+            fPoint{ 50,50 },
+            fPoint{ 50 + 100 * static_cast<float>(std::sin(time)),
+                    50 + 100 * static_cast<float>(std::cos(time)) } });
     }
     void onMouseMove(const MouseMoveEvent& event) override {
         cout << "Mouse moved to " << event.position() << endl;
