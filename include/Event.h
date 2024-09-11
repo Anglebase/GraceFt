7a94d904c4b1fe3e801fc4355c478fd6eacaab06
Anglebase <1628818324@qq.com>
2024-09-11 12:46:36 +0800

    补全键盘事件信息

--- include/Event.h
+++ include/Event.h
@@ -29,38 +29,131 @@ namespace GFt {
     };
     /// @brief 键盘按键枚举
     enum class Key {
-        A /**< A */, B /**< B */, C /**< C */, D /**< D */, E /**< E */, F /**< F */, G /**< G */, H /**< H */,
-        I /**< I */, J /**< J */, K /**< K */, L /**< L */, M /**< M */, N /**< N */, O /**< O */, P /**< P */,
-        Q /**< Q */, R /**< R */, S /**< S */, T /**< T */, U /**< U */, V /**< V */, W /**< W */, X /**< X */,
-        Y /**< Y */, Z /**< Z */,
-        _0 /**< 主按键区数字键 0 */, _1 /**< 主按键区数字键 1 */, _2 /**< 主按键区数字键 2 */, _3 /**< 主按键区数字键 3 */,
-        _4 /**< 主按键区数字键 4 */, _5 /**< 主按键区数字键 5 */, _6 /**< 主按键区数字键 6 */, _7 /**< 主按键区数字键 7 */,
-        _8 /**< 主按键区数字键 8 */, _9 /**< 主按键区数字键 9 */,
-        F1 /**< F1 */, F2 /**< F2 */, F3 /**< F3 */, F4 /**< F4 */, F5 /**< F5 */, F6 /**< F6 */,
-        F7 /**< F7 */, F8 /**< F8 */, F9 /**< F9 */, F10 /**< F10 */, F11 /**< F11 */, F12 /**< F12 */,
-        Space /**< 空格 */, Enter /**< Enter */, Escape /**< Esc */, Backspace /**< Backspace */, Tab /**< Tab */,
-        Left /**< 左箭头 */, Right /**< 右箭头 */, Up /**< 上箭头 */, Down /**< 下箭头 */,
-        Insert /**< Insert */, Delete /**< Delete */, Home /**< Home */,
-        End /**< End */, PageUp /**< PageUp */, PageDown /**< PageDown */, NumLock /**< NumLock */,
-        Numpad0 /**< 小键盘数字键 0 */, Numpad1 /**< 小键盘数字键 1 */, Numpad2 /**< 小键盘数字键 2 */,
-        Numpad3 /**< 小键盘数字键 3 */, Numpad4 /**< 小键盘数字键 4 */, Numpad5 /**< 小键盘数字键 5 */,
-        Numpad6 /**< 小键盘数字键 6 */, Numpad7 /**< 小键盘数字键 7 */, Numpad8 /**< 小键盘数字键 8 */,
-        Numpad9 /**< 小键盘数字键 9 */,
-        NumpadDivide /**< 小键盘 / */, NumpadMultiply /**< 小键盘 * */,
-        NumpadMinus /**< 小键盘 - */, NumpadPlus /**< 小键盘 + */,
-        NumpadEnter /**< 小键盘 Enter */, NumpadDot /**< 小键盘 . */,
-        CapsLock /**< CapsLock */, ScrollLock /**< ScrollLock */,
-        LeftShift /**< 左Shift */, RightShift /**< 右Shift */,
-        LeftControl /**< 左Ctrl */, RightControl /**< 右Ctrl */,
-        LeftAlt /**< 左Alt */, RightAlt /**< 右Alt */,
-        LeftSuper /**< 左Win */, RightSuper /**< 右Win */,
-        Menu /**< Menu */,
-        Equal /**< 符号键 = (等号/加号)*/, Minus /**< 符号键 - (减号/下划线)*/,
-        LeftBracket /**< 符号键 [ (左中括号/左大括号)*/, RightBracket /**< 符号键 ] (右中括号/右大括号))*/,
-        Semicolon /**< 符号键 ; (分号/冒号)*/, Quote /**< 符号键 ' (单引号/双引号)*/,
-        Comma /**<符号键 , (逗号/左尖括号)*/, Period /**<符号键 . (句号/右尖括号)*/,
-        Slash /**< 符号键 / (斜杠/问号)*/, Backslash /**< 符号键 \ (反斜杠/竖线)*/,
-        Tilde /**< 符号键 ` (反引号/波浪号)*/, Unknown /**< 未知按键 */,
+        LeftMouse = 0x01,    ///< 鼠标左键
+        RightMouse = 0x02,   ///< 鼠标右键
+        MiddleMouse = 0x04,  ///< 鼠标中键
+        Backspace = 0x08,    ///< Backspace 键
+        Tab = 0x09,          ///< Tab 键
+        Enter = 0x0d,        ///< Enter 键
+        Shift = 0x10,        ///< Shift 键
+        Control = 0x11,      ///< Control 键
+        Menu = 0x12,         ///< Menu 键
+        Pause = 0x13,        ///< Pause 键
+        Capslock = 0x14,     ///< Capslock 键 (大写锁定键)
+        Esc = 0x1b,          ///< Esc 键
+        Space = 0x20,        ///< 空格键
+
+        Pageup = 0x21,       ///< Pageup 键
+        Pagedown = 0x22,     ///< Pagedown 键
+        End = 0x23,          ///< End 键
+        Home = 0x24,         ///< Home 键
+
+        Left = 0x25,         ///< 左方向键
+        Up = 0x26,           ///< 上方向键
+        Right = 0x27,        ///< 右方向键
+        Down = 0x28,         ///< 下方向键
+
+        Print = 0x2a,        ///< Print 键
+        Snapshot = 0x2c,     ///< Snapshot 键
+        Insert = 0x2d,       ///< Insert 键
+        Delete = 0x2e,       ///< Delete 键
+
+        _0 = 0x30,           ///< 主键盘 0 键
+        _1 = 0x31,           ///< 主键盘 1 键
+        _2 = 0x32,           ///< 主键盘 2 键
+        _3 = 0x33,           ///< 主键盘 3 键
+        _4 = 0x34,           ///< 主键盘 4 键
+        _5 = 0x35,           ///< 主键盘 5 键
+        _6 = 0x36,           ///< 主键盘 6 键
+        _7 = 0x37,           ///< 主键盘 7 键
+        _8 = 0x38,           ///< 主键盘 8 键
+        _9 = 0x39,           ///< 主键盘 9 键
+
+        A = 0x41,            ///< A 键
+        B = 0x42,            ///< B 键
+        C = 0x43,            ///< C 键
+        D = 0x44,            ///< D 键
+        E = 0x45,            ///< E 键
+        F = 0x46,            ///< F 键
+        G = 0x47,            ///< G 键
+        H = 0x48,            ///< H 键
+        I = 0x49,            ///< I 键
+        J = 0x4a,            ///< J 键
+        K = 0x4b,            ///< K 键
+        L = 0x4c,            ///< L 键
+        M = 0x4d,            ///< M 键
+        N = 0x4e,            ///< N 键
+        O = 0x4f,            ///< O 键
+        P = 0x50,            ///< P 键
+        Q = 0x51,            ///< Q 键
+        R = 0x52,            ///< R 键
+        S = 0x53,            ///< S 键
+        T = 0x54,            ///< T 键
+        U = 0x55,            ///< U 键
+        V = 0x56,            ///< V 键
+        W = 0x57,            ///< W 键
+        X = 0x58,            ///< X 键
+        Y = 0x59,            ///< Y 键
+        Z = 0x5a,            ///< Z 键
+        LeftWin = 0x5b,      ///< 左 Windows 键
+        RightWin = 0x5c,     ///< 右 Windows 键
+
+        Sleep = 0x5f,        ///< Sleep 键
+
+        Num0 = 0x60,         ///< 小键盘 0 键
+        Num1 = 0x61,         ///< 小键盘 1 键
+        Num2 = 0x62,         ///< 小键盘 2 键
+        Num3 = 0x63,         ///< 小键盘 3 键
+        Num4 = 0x64,         ///< 小键盘 4 键
+        Num5 = 0x65,         ///< 小键盘 5 键
+        Num6 = 0x66,         ///< 小键盘 6 键
+        Num7 = 0x67,         ///< 小键盘 7 键
+        Num8 = 0x68,         ///< 小键盘 8 键
+        Num9 = 0x69,         ///< 小键盘 9 键
+
+        Multiply = 0x6a,     ///< 小键盘 * 键
+        Add = 0x6b,          ///< 小键盘 + 键
+        Separator = 0x6c,    ///< 小键盘 , 键
+        Subtract = 0x6d,     ///< 小键盘 - 键
+        Decimal = 0x6e,      ///< 小键盘 . 键
+        Divide = 0x6f,       ///< 小键盘 / 键
+
+        F1 = 0x70,           ///< F1 键
+        F2 = 0x71,           ///< F2 键
+        F3 = 0x72,           ///< F3 键
+        F4 = 0x73,           ///< F4 键
+        F5 = 0x74,           ///< F5 键
+        F6 = 0x75,           ///< F6 键
+        F7 = 0x76,           ///< F7 键
+        F8 = 0x77,           ///< F8 键
+        F9 = 0x78,           ///< F9 键
+        F10 = 0x79,          ///< F10 键
+        F11 = 0x7a,          ///< F11 键
+        F12 = 0x7b,          ///< F12 键
+
+        Numlock = 0x90,      ///< Numlock 键
+        Scrolllock = 0x91,   ///< Scrolllock 键
+
+        LeftShift = 0xa0,    ///< 左 Shift 键
+        RightShift = 0xa1,   ///< 右 Shift 键
+        LeftControl = 0xa2,  ///< 左 Control 键
+        RightControl = 0xa3, ///< 右 Control 键
+        LeftMenu = 0xa4,     ///< 左 Menu 键
+        RightMenu = 0xa5,    ///< 右 Menu 键
+
+        Semicolon = 0xba,    ///< ;: 键
+        Plus = 0xbb,         ///< =+ 键
+        Comma = 0xbc,        ///< ,< 键
+        Minus = 0xbd,        ///< -_ 键
+        Period = 0xbe,       ///< .> 键
+        Slash = 0xbf,        ///< /? 键
+        Tilde = 0xc0,        ///< `~ 键
+        Lbrace = 0xdb,       ///< [{ 键
+        Backslash = 0xdc,    ///< \| 键
+        Rbrace = 0xdd,       ///< ]} 键
+        Quote = 0xde,        ///< '" 键
+
+        ImeProcess = 0xe5    ///< IME 处理键
     };
     /// @}
     /// @}
@@ -105,10 +198,12 @@ namespace GFt {
     /// @brief 键盘事件基类
     class KeyboardEvent : public Event {
         Key key_;
+        bool shift_ = false;
+        bool ctrl_ = false;
 
     public:
         /// @cond IGNORE
-        KeyboardEvent(Key key);
+        KeyboardEvent(Key key, bool shift, bool control);
         virtual ~KeyboardEvent() = default;
         /// @endcond
         /// @brief 获取键盘按键
@@ -166,7 +261,7 @@ namespace GFt {
     class KeyPressEvent : public KeyboardEvent {
     public:
         /// @cond IGNORE
-        KeyPressEvent(Key key);
+        KeyPressEvent(Key key, bool shift, bool control);
         virtual ~KeyPressEvent() = default;
         /// @endcond
     };
@@ -174,7 +269,7 @@ namespace GFt {
     class KeyReleaseEvent : public KeyboardEvent {
     public:
         /// @cond IGNORE
-        KeyReleaseEvent(Key key);
+        KeyReleaseEvent(Key key, bool shift, bool control);
         virtual ~KeyReleaseEvent() = default;
         /// @endcond
     };
--- src/Event.cpp
+++ src/Event.cpp
@@ -5,7 +5,7 @@ namespace GFt {
     void Event::stopPropagation() { stopPropagation_ = true; }
     MouseEvent::MouseEvent(const iPoint& position) : position_(position) {}
     const iPoint& MouseEvent::position() const { return position_; }
-    KeyboardEvent::KeyboardEvent(Key key) : key_(key) {}
+    KeyboardEvent::KeyboardEvent(Key key, bool shift, bool ctrl) : key_(key), shift_(shift), ctrl_(ctrl) {}
     Key KeyboardEvent::key() const { return key_; }
     MouseButtonEvent::MouseButtonEvent(const iPoint& position, MouseButton button)
         : MouseEvent(position), button_(button) {}
@@ -14,8 +14,8 @@ namespace GFt {
         : MouseEvent(position), wheel_(wheel) {}
     MouseWheel MouseWheelEvent::wheel() const { return wheel_; }
     MouseMoveEvent::MouseMoveEvent(const iPoint& position) : MouseEvent(position) {}
-    KeyPressEvent::KeyPressEvent(Key key) : KeyboardEvent(key) {}
-    KeyReleaseEvent::KeyReleaseEvent(Key key) : KeyboardEvent(key) {}
+    KeyPressEvent::KeyPressEvent(Key key, bool shift, bool ctrl) : KeyboardEvent(key, shift, ctrl) {}
+    KeyReleaseEvent::KeyReleaseEvent(Key key, bool shift, bool ctrl) : KeyboardEvent(key, shift, ctrl) {}
     MouseButtonPressEvent::MouseButtonPressEvent(const iPoint& position, MouseButton button)
         : MouseButtonEvent(position, button) {}
     MouseButtonReleaseEvent::MouseButtonReleaseEvent(const iPoint& position, MouseButton button)
