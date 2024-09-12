effa45c118d64ffdf7ac58d16d8d6a53f835436b
Anglebase <1628818324@qq.com>
2024-09-12 22:27:41 +0800

    更新示例程序

--- examples/MyWindow.cpp
+++ examples/MyWindow.cpp
@@ -18,21 +18,46 @@ using namespace GFt;
 using namespace GFt::literals;
 
 class MyWindow : public Block {
+    std::wstring text_;
 protected:
     // 重写onDraw()方法，绘制窗口内容
     void onDraw(const iRect& rect) override {
         static Graphics g;                                         // 此函数会被频繁调用，因而
         static BrushSet brush(0xC9C9E69F_rgba);                    // 应该将这些量声明为静态变
         static BrushSet brush2(0xB9EFF68A_rgba);                   // 量或成员变量，以降低频繁
-        static TextSet text(0x0_rgb, Font{ L"Consolas", 1.5_em }); // 创建和销毁这些对象的开销
+        static TextSet text(0x0_rgb, Font{ L"Consolas", 1.5_em });   // 创建和销毁这些对象的开销
         g.bindBrushSet(&brush);
         g.bindTextSet(&text);
         g.drawFillRect(fRect{ 10,10,200,100 });
         g.drawText(L"This is MyWindow in GraceFt!", fPoint{ 30,40 });
+        g.drawText(text_, fPoint{ 30,70 }, { L"Consolas", L"仿宋", L"Segoe UI Emoji" });
         g.bindBrushSet(&brush2);
         g.drawFillRect(fRect{ 50,60,200,100 });
         // 调用父类的onDraw()方法，以执行默认的窗口内容绘制
         Block::onDraw(rect);  // 此函数无默认行为，仅作示例
+    }
+    void onMouseButtonPress(const MouseButtonPressEvent& event) override {
+        std::wcout << L"MouseButtonPress" << std::endl;
+        return Block::onMouseButtonPress(event);
+    }
+    void onTextInput(const TextInputEvent& event) override {
+        switch ((wchar_t)event.character()) {
+        case L'\r':
+        case L'\n':
+            text_.clear();
+            break;
+        case L'\b':
+            if (!text_.empty())
+                text_.pop_back();
+            break;
+        case L'\t':
+            text_ += L"    ";
+            break;
+        default:
+            text_ += event.character();
+            break;
+        }
+        std::wcout << L"TextInput" << std::endl;
     }
 public:
     MyWindow(const iRect& rect, Block* parent = nullptr, int zIndex = 0) : Block(rect, parent, zIndex) {}
