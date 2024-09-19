5f4d7ae5237dfd3b7833d21ecb873358b714c084
Anglebase <1628818324@qq.com>
2024-09-19 22:56:52 +0800

    实现ColumnLayout

---
+++ include/GraceFt/ColumnLayout.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <map>
+#include <GraceFt/Layout.h>
+#include <GraceFt/Block.h>
+
+namespace GFt {
+    class ColumnLayout : public Layout, public Block {
+        int space_{ 0 };
+        std::map<Block*, float> blockLayout_;
+
+        void updateLayout();
+
+    protected:
+        /// @brief 重写onDraw方法，刷新布局
+        void onDraw(const iRect& rect) override;
+
+    public:
+        /// @brief 构造函数
+        /// @param rect 布局的矩形区域
+        /// @param parent 父对象
+        /// @param zIndex 所在层级
+        ColumnLayout(const iRect& rect, Block* parent = nullptr, int zIndex = 0);
+        ~ColumnLayout() override;
+
+        /// @brief 添加参与布局的块对象
+        /// @param block 块对象
+        /// @param widthProportion 块的宽度占比，默认1.f
+        void addLayout(Block* block, float widthProportion = 1.f);
+        /// @brief 移除参与布局的块对象
+        /// @param block 块对象
+        void removeLayout(Block* block);
+
+        /// @brief 设置布局之间的间隔宽度
+        /// @param space 间隔宽度
+        void setSpace(int space);
+        /// @brief 获取布局之间的间隔宽度
+        /// @return 间隔宽度
+        int getSpace() const;
+    };
+}
---
+++ src/ColumnLayout.cpp
@@ -0,0 +1,54 @@
+#include "ColumnLayout.h"
+
+namespace GFt {
+    void ColumnLayout::updateLayout() {
+        // 计算所有block的高度占比之和
+        float sum = 0.0f;
+        for (auto& [block, heightPro] : blockLayout_)
+            sum += heightPro;
+        // 计算每个block的高度、高度、y坐标
+        for (auto& [block, heightPro] : blockLayout_) {
+            block->rect().height() = (heightPro / sum) *
+                (rect().height() - (space_ * (blockLayout_.size() - 1)) - (getTopPadding() + getBottomPadding()));
+            block->rect().width() = rect().width() - (getLeftPadding() + getRightPadding());
+            block->rect().x() = getLeftPadding();
+        }
+        // 计算每个block的x坐标
+        float y = getTopPadding();
+        using Iter = std::map<Block*, float>::reverse_iterator;
+        for (Iter iter = blockLayout_.rbegin(); iter != blockLayout_.rend(); ++iter) {
+            auto& [block, heightPro] = *iter;
+            block->rect().y() = y;
+            y += block->rect().height() + space_;
+        }
+    }
+
+    void GFt::ColumnLayout::onDraw(const iRect& rect) {
+        if (shouleUpdateLayout())
+            updateLayout();
+        return Block::onDraw(rect);
+    }
+    ColumnLayout::ColumnLayout(const iRect& rect, Block* parent, int zIndex)
+        : Block(rect, parent, zIndex) {}
+    ColumnLayout::~ColumnLayout() = default;
+
+    void ColumnLayout::addLayout(Block* block, float widthProportion) {
+        blockLayout_[block] = widthProportion;
+        this->addChild(block);
+        setShouleUpdateLayout();
+    }
+    void ColumnLayout::removeLayout(Block* block) {
+        if (blockLayout_.find(block) == blockLayout_.end())
+            return;
+        blockLayout_.erase(block);
+        this->removeChild(block);
+        setShouleUpdateLayout();
+    }
+    void ColumnLayout::setSpace(int space) {
+        space_ = space;
+        setShouleUpdateLayout();
+    }
+    int ColumnLayout::getSpace() const {
+        return space_;
+    }
+}
