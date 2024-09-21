856703110f8be86f96329703225aaffc653b2ece
Anglebase <1628818324@qq.com>
2024-09-21 23:06:25 +0800

    TEST

--- tests/Graphics.cpp
+++ tests/Graphics.cpp
@@ -2,26 +2,28 @@
 #include <Graphics.h>
 #include <Window.h>
 #include <Tools.h>
+#include <LMath.hpp>
 
 using namespace GFt::literals;
 using namespace GFt;
 
 class MyWindow : public Block {
+    PixelMap pixmap;
 protected:
     void onDraw(const iRect& rect) override {
         static Graphics g;
         BrushSet bs(0x9f9fefcd_rgba);
-        auto frect = iRect{ iPoint{50,50}, rect.size() - iSize{100,100} };
-        iPolygon ploy;
-        g.bindBrushSet(&bs);
-        ploy.addPoint(iPoint{ 100, 100 });
-        ploy.addPoint(iPoint{ 100, 500 });
-        ploy.addPoint(iPoint{ 400, 500 });
-        ploy.addPoint(iPoint{ 500, 100 });
-        g.drawFillPolygon(ploy);
+        g.setTransform(
+            rotate((fVec2)fPoint(50.f, 50.f), radians(45.0f))
+            * scale((fVec2)fPoint(50.f, 50.f), makefVec2(0.5f, 0.5f))
+            * translate((fVec2)fPoint(100.f, 100.f))
+            );
+        g.drawImage(iPoint(50, 50), pixmap);
     }
 public:
-    MyWindow(const iRect& rect, Block* parent = nullptr, int zIndex = 0) : Block(rect, parent, zIndex) {}
+    MyWindow(const iRect& rect, Block* parent = nullptr, int zIndex = 0) : Block(rect, parent, zIndex) {
+        pixmap = PixelMap::loadFromFile(LR"(C:\Users\Lenovo\Project\.Github\GraceFt_\wall.jpg)");
+    }
 };
 
 int main() {
