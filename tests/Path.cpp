53cb10410fd7794cc217323257ac6872d88cb043
Anglebase <1628818324@qq.com>
2024-09-21 21:38:32 +0800

    测试

--- tests/Path.cpp
+++ tests/Path.cpp
@@ -5,6 +5,7 @@
 #include <Graphics.h>
 #include <LMath.hpp>
 #include <Button.h>
+#include <Geometry.hpp>
 
 using namespace GFt;
 using namespace GFt::Widget;
@@ -13,18 +14,26 @@ class PathTest : public Block {
 protected:
     void onDraw(const iRect& rect) override {
         Graphics g;
-        Path path;
-        path.addText(L"传达", iPoint(200, 200), Font(L"微软雅黑", 50));
-        g.drawCircle({ iPoint(400, 400), 30 });
-        g.drawPath(path, iPoint(0, 0));
-        g.drawLine({ iPoint{ 0,0 }, iPoint{ 300,300 } });
-        g.setTransform(
-            scale(makefVec2(200.f, 200.f), makefVec2(2.f, 2.f))
-            * rotate(makefVec2(200.f, 200.f), radians(135.f))
-            * translate(makefVec2(200.f, 200.f))
-        );
-        g.drawPath(path, iPoint(0, 0));
-        g.drawLine({ iPoint{ 0,0 }, iPoint{ 300,300 } });
+        // Path path;
+        // path.addText(L"传达", iPoint(200, 200), Font(L"微软雅黑", 50));
+        // path.transformBy(
+        //     scale(makefVec2(200.f, 200.f), makefVec2(2.f, 2.f))
+        //     * rotate(makefVec2(200.f, 200.f), radians(30.f))
+        // );
+        // path.addText(L"信息", iPoint(200, 200), Font(L"微软雅黑", 50));
+        // g.drawCircle({ iPoint(400, 400), 30 });
+        // g.drawPath(path, iPoint(0, 0));
+        // g.drawLine({ iPoint{ 0,0 }, iPoint{ 300,300 } });
+        // g.setTransform(
+        //     scale(makefVec2(200.f, 200.f), makefVec2(2.f, 2.f))
+        //     * rotate(makefVec2(200.f, 200.f), radians(135.f))
+        //     * translate(makefVec2(200.f, 200.f))
+        // );
+        // g.drawPath(path, iPoint(0, 0));
+        fLine line{ iPoint{ 0,0 }, iPoint{ 300,300 } };
+        g.drawLine(line);
+        g.drawCircle(iCircle{ iPoint{ 100,200 }, 5 });
+        g.drawLine(centerSymmetric(line, fPoint{ 100,200 }));
     }
 public:
     PathTest(const iRect& rect, Block* parent = nullptr, int zIndex = 0)
