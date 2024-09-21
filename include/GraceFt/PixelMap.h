6a8eb097086c2e3d6587c96f4a6cf8432b45c35b
Anglebase <1628818324@qq.com>
2024-09-21 23:05:45 +0800

    完善位图类支持

--- include/GraceFt/PixelMap.h
+++ include/GraceFt/PixelMap.h
@@ -5,7 +5,7 @@
 #include <GraceFt/Size.hpp>
 #include <GraceFt/Rect.hpp>
 
-namespace GFt{
+namespace GFt {
     /// @class PixelMap
     /// @brief 位图类
     /// @ingroup 接口类型
@@ -26,5 +26,26 @@ namespace GFt{
         /// @param rect 裁剪区域
         /// @return 裁剪出的位图
         PixelMap clip(const iRect& rect) const;
+        /// @brief 获取位图大小
+        /// @return 位图大小
+        iSize size() const;
+        /// @brief 为位图的每个像素统一设置Alpha通道
+        /// @param alpha 新的Alpha值
+        void setAlpha(int alpha);
+        /// @brief 将位图保存到文件
+        /// @param filename 文件名
+        /// @details 支持的格式：PNG、BMP，其它的将保存为PNG
+        /// @param withAlpha 是否保存Alpha通道
+        void saveToFile(const std::wstring& filename, bool withAlpha = false) const;
+    public:
+        /// @brief 加载位图文件
+        /// @param filename 文件位置
+        /// @details 支持的格式：PNG、BMP、JPG、GIF、EMF、WMF、ICO
+        /// @return 位图对象
+        static PixelMap loadFromFile(const std::wstring& filename);
+        /// @brief 从窗口中加载位图
+        /// @param rect 窗口位置
+        /// @return 位图对象
+        static PixelMap loadFromWindow(const iRect& rect);
     };
 }
--- src/PixelMap.cpp
+++ src/PixelMap.cpp
@@ -1,6 +1,7 @@
 #include "PixelMap.h"
 
 #include <ege.h>
+#include <exception>
 
 #define IMG(x) (static_cast<PIMAGE>(x))
 
@@ -47,5 +48,46 @@ namespace GFt {
         PixelMap result(rect.size());
         putimage(IMG(result.pixmap_), 0, 0, rect.width(), rect.height(), IMG(pixmap_), rect.x(), rect.y());
         return result;
+    }
+    iSize PixelMap::size() const {
+        return iSize(getwidth(IMG(pixmap_)), getheight(IMG(pixmap_)));
+    }
+    void PixelMap::setAlpha(int alpha) {
+        ege_setalpha(alpha, IMG(pixmap_));
+    }
+    void PixelMap::saveToFile(const std::wstring& filename, bool withAlpha) const {
+        auto ret = saveimage(IMG(pixmap_), filename.c_str(), withAlpha);
+        if (ret != grOk)
+            throw std::runtime_error("Failed to save image");
+    }
+    PixelMap PixelMap::loadFromFile(const std::wstring& filename) {
+        PixelMap result;
+        auto ret = getimage(IMG(result.pixmap_), filename.c_str());
+        switch (ret) {
+        case grOk:
+            return result;
+        case grAllocError:
+            throw std::runtime_error("Memory allocation failed");
+        case grFileNotFound:
+            throw std::runtime_error("File not found");
+        case grIOerror:
+            throw std::runtime_error("Read failed");
+        default:
+            throw std::runtime_error("Unknown error");
+        }
+    }
+    PixelMap PixelMap::loadFromWindow(const iRect& rect) {
+        PixelMap result(rect.size());
+        auto ret = getimage(IMG(result.pixmap_), rect.x(), rect.y(), rect.width(), rect.height());
+        switch (ret) {
+        case grOk:
+            return result;
+        case grAllocError:
+            throw std::runtime_error("Memory allocation failed");
+        case grIOerror:
+            throw std::runtime_error("Read failed");
+        default:
+            throw std::runtime_error("Unknown error");
+        }
     }
 }
