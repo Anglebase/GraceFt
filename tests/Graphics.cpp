baf145a70a0b2ea429349e4346e6ae91b2cda44f
Anglebase <1628818324@qq.com>
2024-09-22 22:51:37 +0800

    TEST

--- tests/Graphics.cpp
+++ tests/Graphics.cpp
@@ -9,20 +9,24 @@ using namespace GFt;
 
 class MyWindow : public Block {
     PixelMap pixmap;
+    Texture texture;
 protected:
     void onDraw(const iRect& rect) override {
         static Graphics g;
         BrushSet bs(0x9f9fefcd_rgba);
+        bs.setTexture(texture, iRect(0, 0, 100, 100));
+        g.bindBrushSet(&bs);
         g.setTransform(
             rotate((fVec2)fPoint(50.f, 50.f), radians(45.0f))
             * scale((fVec2)fPoint(50.f, 50.f), makefVec2(0.5f, 0.5f))
             * translate((fVec2)fPoint(100.f, 100.f))
             );
-        g.drawImage(iPoint(50, 50), pixmap);
+        g.drawFillRect(iRect(50, 50, 200, 200));
     }
 public:
-    MyWindow(const iRect& rect, Block* parent = nullptr, int zIndex = 0) : Block(rect, parent, zIndex) {
-        pixmap = PixelMap::loadFromFile(LR"(C:\Users\Lenovo\Project\.Github\GraceFt_\wall.jpg)");
+    MyWindow(const iRect& rect, Block* parent = nullptr, int zIndex = 0)
+    : Block(rect, parent, zIndex), texture(LR"(C:\Users\Lenovo\Project\OpenGL\wall.jpg)") {
+        pixmap = PixelMap::loadFromFile(LR"(C:\Users\Lenovo\Project\OpenGL\wall.jpg)");
     }
 };
 
--- tests/Window.cpp
+++ tests/Window.cpp
@@ -1,12 +1,39 @@
 #include <Window.h>
+#include <Application.h>
+#include <Button.h>
 #include <iostream>
 
 using namespace std;
 using namespace GFt;
+using namespace GFt::literals;
+using namespace GFt::Widget;
 
 int main() {
-    Block block{ iRect{ 30,30,800,600 } };
+    Block block{ iRect{ 50,50,800,600 } };
+
+    Button btn1{ L"置顶窗口" , iRect{ 10,10,100,50 },&block };
+    Button btn2{ L"取消置顶", iRect{ 120,10,100,50 },&block };
+    Button btn3{ L"移除边框", iRect{ 230,10,100,50 },&block };
+    Button btn4{ L"显示边框", iRect{ 340,10,100,50 },&block };
+    Button btn5{ L"透明", iRect{ 450,10,100,50 },&block };
+    Button btn6{ L"不透明", iRect{ 560,10,100,50 },&block };
+    Button btn7{ L"最小化", iRect{ 670,10,100,50 },&block };
+    Button btn8{ L"最大化", iRect{ 10,65,100,50 },&block };
+    Button btn9{ L"还原", iRect{ 120,65,100,50 },&block };
+
+    btn1.onClicked.connect([]{ Window::window()->setTopMost(true); });
+    btn2.onClicked.connect([]{ Window::window()->setTopMost(false); });
+    btn3.onClicked.connect([]{ Window::window()->setFrameless(true); });
+    btn4.onClicked.connect([]{ Window::window()->setFrameless(false); });
+    btn5.onClicked.connect([]{ Window::window()->setAlpha(.8f); });
+    btn6.onClicked.connect([]{ Window::window()->setAlpha(1.f); });
+    btn7.onClicked.connect([]{ Window::window()->minimize(); });
+    btn8.onClicked.connect([]{ Window::window()->maximize(); });
+    btn9.onClicked.connect([]{ Window::window()->restore(); });
+
     Window* window = Window::createWindow(&block);
 
-    return 0;
+    Application app{ window };
+    window->show();
+    return app.run();
 }
