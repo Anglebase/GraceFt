f1f4add127ed9fe1c9ed4b1100c0228fdde783c1
Anglebase <1628818324@qq.com>
2024-09-23 19:11:55 +0800

    测试

--- tests/Window.cpp
+++ tests/Window.cpp
@@ -18,8 +18,9 @@ int main() {
     Button btn5{ L"透明", iRect{ 450,10,100,50 },&block };
     Button btn6{ L"不透明", iRect{ 560,10,100,50 },&block };
     Button btn7{ L"最小化", iRect{ 670,10,100,50 },&block };
-    Button btn8{ L"最大化", iRect{ 10,65,100,50 },&block };
+    Button btn8{ L"全屏", iRect{ 10,65,100,50 },&block };
     Button btn9{ L"还原", iRect{ 120,65,100,50 },&block };
+    Button btn10{ L"最大化", iRect{ 230,65,100,50 },&block };
 
     btn1.onClicked.connect([]{ Window::window()->setTopMost(true); });
     btn2.onClicked.connect([]{ Window::window()->setTopMost(false); });
@@ -28,8 +29,9 @@ int main() {
     btn5.onClicked.connect([]{ Window::window()->setAlpha(.8f); });
     btn6.onClicked.connect([]{ Window::window()->setAlpha(1.f); });
     btn7.onClicked.connect([]{ Window::window()->minimize(); });
-    btn8.onClicked.connect([]{ Window::window()->maximize(); });
+    btn8.onClicked.connect([]{ Window::window()->fullscreen(); });
     btn9.onClicked.connect([]{ Window::window()->restore(); });
+    btn10.onClicked.connect([]{ Window::window()->maximize(); });
 
     Window* window = Window::createWindow(&block);
 
