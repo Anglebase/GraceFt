c62a98345dde786b250a53d23dd6e9a57bc54422
Anglebase <1628818324@qq.com>
2024-09-23 16:47:43 +0800

    测试

--- tests/Graphics.cpp
+++ tests/Graphics.cpp
@@ -14,18 +14,27 @@ protected:
     void onDraw(const iRect& rect) override {
         static Graphics g;
         BrushSet bs(0x9f9fefcd_rgba);
-        bs.setTexture(texture, iRect(0, 0, 100, 100));
+        // bs.setTexture(texture, iRect(0, 0, 100, 100));
+        // bs.setLinearGradient(iPoint{ 50,50 }, 0xff00_rgb, iPoint{ 150,150 }, 0xff_rgb);
+        // bs.setRadialGradient(iPoint{ 150,150 }, 0xff00_rgb, iRect{ 50,50,200,200 }, 0xff_rgb);
+        bs.setPolygonGradient(iPoint{ 125,125 }, 0xffff_rgb,
+            std::vector<fPoint>{ iPoint{ 50,50 }, iPoint{ 50,200 }, iPoint{ 200,200 }, iPoint{ 200,50 } },
+            std::vector<Color> { 0xff0000_rgb, 0xff00_rgb, 0xff_rgb, 0xff00ff_rgb });
         g.bindBrushSet(&bs);
-        g.setTransform(
-            rotate((fVec2)fPoint(50.f, 50.f), radians(45.0f))
-            * scale((fVec2)fPoint(50.f, 50.f), makefVec2(0.5f, 0.5f))
-            * translate((fVec2)fPoint(100.f, 100.f))
-            );
-        g.drawFillRect(iRect(50, 50, 200, 200));
+        g.setAntiAliasing(true);
+        Path path;
+        path.addText(L"Hello, world!", iPoint{ 0,0 }, Font{ L"宋体", 4_em });
+        // g.setTransform(
+        //     rotate((fVec2)fPoint(0, 0), radians(180.0f))
+        //     * translate((fVec2)fPoint(100, 100))
+        // );
+        // g.drawFillRect(iRect(50, 50, 200, 200));
+        // g.drawFillRect(iRect(iPoint{0,0}, rect.size()));
+        g.drawFillPath(path, iPoint{ 20,20 });
     }
 public:
     MyWindow(const iRect& rect, Block* parent = nullptr, int zIndex = 0)
-    : Block(rect, parent, zIndex), texture(LR"(C:\Users\Lenovo\Project\OpenGL\wall.jpg)") {
+        : Block(rect, parent, zIndex), texture(LR"(C:\Users\Lenovo\Project\OpenGL\wall.jpg)") {
         pixmap = PixelMap::loadFromFile(LR"(C:\Users\Lenovo\Project\OpenGL\wall.jpg)");
     }
 };
@@ -35,5 +44,6 @@ int main() {
     auto window = Window::createWindow(&root);
     Application app(window);
     window->show();
+    window->setTopMost(true);
     return app.exec();
 }
