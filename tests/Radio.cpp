6dc4fa57bebd9f4b29931f96f959fa1c7175b872
Anglebase <1628818324@qq.com>
2024-09-29 14:23:24 +0800

    测试

---
+++ tests/Radio.cpp
@@ -0,0 +1,34 @@
+#include <Application.h>
+#include <Radio.h>
+#include <Block.h>
+#include <Window.h>
+#include <MainWindow.h>
+
+using namespace GFt;
+using namespace GFt::Widget;
+using namespace GFt::literals;
+
+int main(){
+    MainWindow root{L"Radio Test", iRect{100, 100, 800, 600}};
+    RadioManager radioManager;
+    RadioBox radioBox1{radioManager, iRect{50, 100, 100, 20}, &root};
+    RadioBox radioBox2{radioManager, iRect{50, 130, 100, 20}, &root};
+    RadioBox radioBox3{radioManager, iRect{50, 160, 100, 20}, &root};
+    RadioBox radioBox4{radioManager, iRect{50, 190, 100, 20}, &root};
+
+    radioBox1.text() = L"RadioBox1";
+    radioBox2.text() = L"RadioBox2";
+    radioBox3.text() = L"RadioBox3";
+    radioBox4.text() = L"RadioBox4";
+
+    radioBox1.onCheckChanged.connect([](bool){
+        std::cout << "FPS: " << Application::getRealFps() << std::endl;
+        std::cout << "Event: " << Application::getEventTime() << std::endl;
+        std::cout << "Render: " << Application::getRenderTime() << std::endl;
+    });
+
+    Window* window = Window::createWindow(&root);
+    Application app{ window };
+    window->show();
+    return app.run();
+}
---
+++ tests/UIRadio.cpp
@@ -0,0 +1,61 @@
+#include <GraceFt/UI.h>
+#include <GraceFt/Application.h>
+
+using namespace GFt;
+using namespace GFt::Widget;
+using namespace GFt::UI;
+
+int main() {
+    XMainWindow{
+        .name = "Root",
+        .title = L"UI Radio Test",
+        .rect = iRect{100, 100, 800, 600},
+        .content = [](MainWindow& it) {
+            XColumnLayout{
+                .name = "ColumnLayout",
+                .rect = iRect{0, 0, 800, 600},
+                .parent = it,
+                .content = [](ColumnLayout& it) {
+                    Window::onWindowCreated.connect(
+                        [&it](Window* w) {
+                            w->onSizeChanged.connect(&it, &ColumnLayout::setSize);
+                        }
+                    );
+                    static RadioManager manager;
+                    XRadioBox{
+                        .name = "RadioBox1",
+                        .radioManager = manager,
+                        .parent = it,
+                        .content = [](RadioBox& it) {
+                            it.text() = L"RadioBox1";
+                        }
+                    };
+                    it.addItem(BLOCK("RadioBox1"));
+                    XRadioBox{
+                        .name = "RadioBox2",
+                        .radioManager = manager,
+                        .parent = it,
+                        .content = [](RadioBox& it) {
+                            it.text() = L"RadioBox2";
+                        }
+                    };
+                    it.addItem(BLOCK("RadioBox2"));
+                    XRadioBox{
+                        .name = "RadioBox3",
+                        .radioManager = manager,
+                        .parent = it,
+                        .content = [](RadioBox& it) {
+                            it.text() = L"RadioBox3";
+                        }
+                    };
+                    it.addItem(BLOCK("RadioBox3"));
+                }
+            };
+        }
+    };
+
+    Window* window = Window::createWindow(BLOCK("Root"));
+    Application app{ window };
+    window->show();
+    return app.run();
+}
