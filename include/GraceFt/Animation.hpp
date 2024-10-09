cb323727a2433901d2f1aed1c0c1b643d0bda025
Anglebase <1628818324@qq.com>
2024-10-09 21:57:41 +0800

    实现动画效果支持

---
+++ include/GraceFt/Animation.hpp
@@ -0,0 +1,187 @@
+#pragma once
+
+#include <chrono>
+#include <functional>
+#include <list>
+#include <cmath>
+#include <GraceFt/Signal.hpp>
+
+namespace GFt {
+    /// @defgroup 动画支持库
+    /// @ingroup 基础设施库
+
+    using TimePoint = std::chrono::time_point<std::chrono::steady_clock>;
+    using TransFunc = std::function<float(float)>;
+    template<typename Type>
+    concept Animatable = requires(Type v) { { v + (v - v) * 1.f } -> std::convertible_to<Type>; };
+    template<typename Type>
+    using Setter = std::function<void(const Type&)>;
+
+    /// @brief 动画状态类型
+    /// @ingroup 动画支持库
+    enum class AnimationStateType {
+        Stop,
+        Play,
+        Pause
+    };
+
+    class AnimationAbstract;
+    /// @brief 动画状态基类
+    /// @ingroup 动画支持库
+    class AnimationState {
+    protected:
+        AnimationAbstract* owner_;
+
+    public:
+        AnimationState(AnimationAbstract* owner);
+        virtual void update(const TimePoint& now) = 0;
+    };
+    /// @brief 动画抽象基类
+    /// @ingroup 动画支持库
+    class AnimationAbstract {
+        friend class AnimationManager;
+        friend class AnimationState;
+        friend class AnimationStop;
+        friend class AnimationPlay;
+        friend class AnimationPause;
+        template<typename Type> requires Animatable<Type> friend class Animation;
+
+        TimePoint start_time_;
+        std::chrono::milliseconds finished_;
+        float duration_ms_;
+        TransFunc trans_func_;
+        AnimationState* state_ = nullptr;
+
+    protected:
+        virtual void playingUpdate(const TimePoint& now) = 0;
+
+    public:
+        AnimationAbstract(const TimePoint& start_time, float ms, const TransFunc& trans_func);
+        ~AnimationAbstract();
+        void update(const TimePoint& now);
+
+        /// @brief 设置动画状态为停止
+        void setStop();
+        /// @brief 设置动画状态为播放
+        void setPlay();
+        /// @brief 设置动画状态为暂停
+        void setPause();
+        /// @brief 判断动画是否处于播放状态
+        bool isPlaying() const;
+        /// @brief 判断动画是否处于暂停状态
+        bool isPaused() const;
+        /// @brief 判断动画是否处于停止状态
+        bool isStopped() const;
+
+        Signal<AnimationStateType, AnimationStateType> onStateChanged;  ///< 动画状态改变信号
+        Signal<void> onFinished;  ///< 动画进度改变信号
+    };
+    /// @brief 停止状态
+    /// @ingroup 动画支持库
+    class AnimationStop : public AnimationState {
+    public:
+        AnimationStop(AnimationAbstract* owner);
+        void update(const TimePoint& now) override;
+    };
+    /// @brief 播放状态
+    /// @ingroup 动画支持库
+    class AnimationPlay : public AnimationState {
+    public:
+        AnimationPlay(AnimationAbstract* owner);
+        void update(const TimePoint& now) override;
+    };
+    /// @brief 暂停状态
+    /// @ingroup 动画支持库
+    class AnimationPause : public AnimationState {
+    public:
+        AnimationPause(AnimationAbstract* owner);
+        void update(const TimePoint& now) override;
+    };
+    /// @brief 动画参数结构体
+    /// @ingroup 动画支持库
+    template<typename Type>
+        requires Animatable<Type>
+    struct AnimationParams {
+        const Setter<Type>& setter;     ///< 动画值的 setter 方法
+        const Type& initial;            ///< 动画初始值
+        const Type& target;             ///< 动画目标值
+        const float& duration;          ///< 动画持续时间（毫秒）
+        TransFunc trans_func;           ///< 动画过渡函数
+    };
+    /// @brief 动画模板类
+    /// @tparam Type 动画值类型
+    /// @ingroup 动画支持库
+    template<typename Type>
+        requires Animatable<Type>
+    class Animation : public AnimationAbstract {
+        Type start_value_;
+        Type end_value_;
+
+        Setter<Type> setter_;
+    protected:
+        void playingUpdate(const TimePoint& now) override {
+            using namespace std::chrono;
+            finished_ = duration_cast<milliseconds>(now - start_time_);
+            auto elapsed_ns = duration_cast<nanoseconds>(finished_).count();
+            auto progress = elapsed_ns * 1e-6f / duration_ms_;
+            if (progress > 1.f) {
+                setStop();
+                onFinished();
+                return;
+            }
+            setter_(start_value_ + (end_value_ - start_value_) * trans_func_(progress));
+        }
+    public:
+        /// @brief 构造函数
+        /// @param params 动画参数结构体
+        Animation(const AnimationParams<Type>& params)
+            : setter_(params.setter), start_value_(params.initial), end_value_(params.target),
+            AnimationAbstract(TimePoint(), params.duration, params.trans_func) {}
+    };
+    /// @brief 动画管理器
+    /// @ingroup 动画支持库
+    class AnimationManager {
+        std::list<AnimationAbstract*> animations_;
+
+        AnimationManager() = default;
+        AnimationManager(const AnimationManager&) = delete;
+        AnimationManager(AnimationManager&&) = delete;
+        AnimationManager& operator=(const AnimationManager&) = delete;
+        AnimationManager& operator=(AnimationManager&&) = delete;
+    public:
+        /// @brief 注册动画对象
+        /// @param animation 动画对象
+        void registerAnimation(AnimationAbstract* animation);
+        /// @brief 注销动画对象
+        /// @param animation 动画对象
+        void unregisterAnimation(AnimationAbstract* animation);
+        /// @brief 更新所有动画对象
+        /// @param now 当前时间
+        void updateAll(const TimePoint& now);
+
+        /// @brief 获取动画管理器实例
+        static AnimationManager& getInstance();
+    };
+
+    /// @brief 过渡函数集合
+    /// @details 此命名空间包含了一些常用的过渡函数，并给出了它们的曲线图像
+    /// @note 自定义过渡函数要求：
+    /// - 函数类型为 `float(float)`
+    /// - 需要保证函数的定义域包含 [0, 1] 区间
+    /// @ingroup 动画支持库
+    namespace TransFuncs {
+        /// @brief 线性过渡函数(默认函数)
+        /// @image html "transfunc/linear.png" "linear 过渡变换曲线"
+        inline float linear(float x) { return x; }
+        /// @image html "transfunc/exp2.png" "exp2 过渡变换曲线"
+        inline float exp2(float x) { return ::std::pow(x, 2.f); }
+        /// @image html "transfunc/exp3.png" "exp3 过渡变换曲线"
+        inline float exp3(float x) { return ::std::pow(x, 3.f); }
+        /// @image html "transfunc/exp4.png" "exp4 过渡变换曲线"
+        inline float exp4(float x) { return ::std::pow(x, 4.f); }
+        /// @image html "transfunc/bezier.png" "bezier 过渡变换曲线"
+        inline float bezier(float x) { return 3.f * x * x - 2.f * x * x * x; }
+        /// @image html "transfunc/easeIn.png" "easeIn 过渡变换曲线"
+        inline float easeIn(float x) { return 1.f - ::std::pow(1.f - x, 2.f); }
+    }
+}
---
+++ src/Animation.cpp
@@ -0,0 +1,89 @@
+#include "Animation.hpp"
+#include <Application.h>
+
+namespace GFt {
+    AnimationState::AnimationState(AnimationAbstract* owner) : owner_(owner) {}
+    AnimationAbstract::AnimationAbstract(
+        const TimePoint& start_time,
+        float ms,
+        const TransFunc& trans_func)
+        : start_time_(start_time),
+        duration_ms_(ms),
+        trans_func_(trans_func ? trans_func : [](float x)->float { return x; }) {
+        state_ = new AnimationStop(this);
+    }
+    AnimationAbstract::~AnimationAbstract() {
+        delete state_;
+    }
+    void AnimationAbstract::update(const TimePoint& now) { if (state_) state_->update(now); }
+    /// @details 若处于停止状态，则此函数无效
+    void AnimationAbstract::setStop() {
+        if (isStopped()) return;
+        if (isPlaying())
+            onStateChanged(AnimationStateType::Play, AnimationStateType::Stop);
+        else if (isPaused())
+            onStateChanged(AnimationStateType::Pause, AnimationStateType::Stop);
+        delete state_;
+        state_ = new AnimationStop(this);
+    }
+    /// @details 若处于播放状态，则此函数无效
+    void AnimationAbstract::setPlay() {
+        if (isPlaying()) return;
+        if (isPaused())
+            onStateChanged(AnimationStateType::Pause, AnimationStateType::Play);
+        else if (isStopped())
+            onStateChanged(AnimationStateType::Stop, AnimationStateType::Play);
+        delete state_;
+        state_ = new AnimationPlay(this);
+    }
+    /// @details 不允许从停止状态直接切换到暂停状态，必须先切换到播放状态再切换到暂停状态
+    ///          若处于停止状态或暂停状态，则此函数无效
+    void AnimationAbstract::setPause() {
+        if (isPaused() || isStopped()) return;
+        if (isPlaying())
+            onStateChanged(AnimationStateType::Play, AnimationStateType::Pause);
+        delete state_;
+        state_ = new AnimationPause(this);
+    }
+    bool AnimationAbstract::isPlaying() const {
+        return dynamic_cast<AnimationPlay*>(state_) != nullptr;
+    }
+    bool AnimationAbstract::isPaused() const {
+        return dynamic_cast<AnimationPause*>(state_) != nullptr;
+    }
+    bool AnimationAbstract::isStopped() const {
+        return dynamic_cast<AnimationStop*>(state_) != nullptr;
+    }
+    AnimationStop::AnimationStop(AnimationAbstract* owner) : AnimationState(owner) {}
+    void AnimationStop::update(const TimePoint& now) { owner_->start_time_ = now; }
+    AnimationPlay::AnimationPlay(AnimationAbstract* owner) : AnimationState(owner) {}
+    void AnimationPlay::update(const TimePoint& now) { owner_->playingUpdate(now); }
+    AnimationPause::AnimationPause(AnimationAbstract* owner) : AnimationState(owner) {}
+    void AnimationPause::update(const TimePoint& now) { owner_->start_time_ = now - owner_->finished_; }
+    void AnimationManager::registerAnimation(AnimationAbstract* animation) {
+        if (std::find(animations_.begin(), animations_.end(), animation) == animations_.end())
+            animations_.push_back(animation);
+    }
+    void AnimationManager::unregisterAnimation(AnimationAbstract* animation) {
+        using Iter = std::list<AnimationAbstract*>::iterator;
+        Iter it = std::remove_if(animations_.begin(), animations_.end(),
+            [animation](AnimationAbstract* a) { return a == animation; });
+        animations_.erase(it, animations_.end());
+    }
+    void AnimationManager::updateAll(const TimePoint& now) {
+        for (auto animation : animations_)
+            animation->update(now);
+    }
+    AnimationManager& AnimationManager::getInstance() {
+        static AnimationManager instance;
+        static bool initialized = false;
+        if (!initialized) {
+            initialized = true;
+            Application::onEventCall.connect([] {
+                using namespace std::chrono;
+                AnimationManager::getInstance().updateAll(steady_clock::now());
+                });
+        }
+        return instance;
+    }
+}
