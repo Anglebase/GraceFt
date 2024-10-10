50f52df48f3c76721a7ae47b73d49114ccee73eb
Anglebase <1628818324@qq.com>
2024-10-10 17:41:12 +0800

    测试

--- tests/Animation.cpp
+++ tests/Animation.cpp
@@ -1,14 +1,17 @@
 #include <Application.h>
 #include <Animation.hpp>
+#include <BlockFocus.h>
 #include <Button.h>
+#include <Geometry.hpp>
 
 using namespace GFt;
 using namespace GFt::Widget;
 using namespace GFt::literals;
 
 class View : public Block {
+    iPoint mousePos_;
 protected:
-    void onDraw(Graphics& g) {
+    void onDraw(Graphics& g) override {
         static BrushSet bs{ 0x999999_rgb };
         g.bindBrushSet(&bs);
         g.drawFillRect({ rect().size() });
@@ -18,20 +21,34 @@ protected:
             anim.isPaused() ? L"暂停状态" : L"无效状态",
             iPoint{ 10, 10 }
         );
+        if (BlockHoverManager::getHoverOn() == this) {
+            g.drawText(L"动画演示对象", mousePos_ + iPoint{ 0,-1_em });
+        }
+    }
+    void onMouseMove(MouseMoveEvent* e) override {
+        mousePos_ = e->absolutePosition() - this->absolutePos();
     }
 public:
     View(const iRect& rect, Block* parent = nullptr, int zIndex = 0)
         : Block(rect, parent, zIndex), anim{
             AnimationParams<int>{
                 .setter = std::bind(&Block::setX, this, std::placeholders::_1),
-                .initial = 100,
+                .initial = rect.x(),
                 .target = 500,
-                .duration = 1000,
-                .trans_func = TransFuncs::easeIn,
+                .duration = 5000,
+                .trans_func = TransFuncs::overDamped(4.f),
             }
         } {
         AnimationManager::getInstance().registerAnimation(&this->anim);
-        // anim.onFinished.connect([this] { this->setX(100); });
+        // anim.onFinished.connect(&anim, &AnimationAbstract::setPlay);
+        anim.onUpdated.connect([this] {
+            auto absPos = Application::getAbsoluteMousePosition();
+            if (contains({ this->absolutePos(), this->rect().size() }, absPos))
+                BlockHoverManager::setHoverOn(this);
+            else
+                BlockHoverManager::setHoverOn(this->getParent());
+            this->mousePos_ = absPos - this->absolutePos();
+            });
     }
     ~View() {
         AnimationManager::getInstance().unregisterAnimation(&this->anim);
@@ -57,6 +74,14 @@ int main() {
     button3.text() = L"终止播放";
     button3.onClicked.connect(&view.anim, &AnimationAbstract::setStop);
 
+    Button button4{ iRect{100,430,100,50}, &root };
+    button4.text() = L"输出FPS";
+    button4.onClicked.connect([] {
+        std::cout << "FPS: " << Application::getRealFps() << std::endl;
+        std::cout << "Event: " << Application::getEventTime() << "us" << std::endl;
+        std::cout << "Render: " << Application::getRenderTime() << "ms" << std::endl;
+        });
+
     view.anim.onStateChanged.connect([&](AnimationStateType before, AnimationStateType after) {
         after == AnimationStateType::Stop
             ? button2.setEnable(false)
@@ -65,6 +90,7 @@ int main() {
 
     auto window = Window::createWindow(&root);
     Application app{ window };
+    std::cout << "Path: " << Application::localPath() << std::endl;
     window->show();
     return app.run();
 }
