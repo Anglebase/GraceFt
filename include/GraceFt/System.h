62944216bdc2650bfea6f08e186879ccff0cc5e9
Anglebase <1628818324@qq.com>
2024-10-12 22:32:51 +0800

    添加系统调用API支持

---
+++ include/GraceFt/System.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <GraceFt/Event.h>
+#include <GraceFt/Point.hpp>
+
+namespace GFt {
+    /// @brief 系统调用接口函数
+    /// @details 系统调用接口相关的函数，它们几乎等同于 Windows API 中的函数原型，但对部分函数添加了 C++ 风格封装
+    /// @ingroup API接口库
+    namespace Sys {
+        /// @brief 获取键盘按键状态(基于消息队列读取)
+        /// @param key 要获取的按键
+        /// @return 按键状态
+        ///         - 若高序位`(x & 0x8000)` 为 1，则表示按键当前处于按下状态；
+        ///         - 若低序位`(x & 0x0001)` 为 1，则表示该按键在上次调用此函数后至少被按下了一次
+        unsigned short getKeyState(Key key);
+        /// @brief 获取键盘按键状态(直接读取物理设备)
+        /// @param key 要获取的按键
+        /// @return 按键状态
+        ///         - 若高序位`(x & 0x8000)` 为 1，则表示按键当前处于按下状态；
+        ///         - 若低序位`(x & 0x0001)` 为 1，则表示该按键在上次调用此函数后至少被按下了一次
+        unsigned short getAsyncKeyState(Key key);
+        /// @brief 获取鼠标的绝对位置
+        /// @return 鼠标位置的坐标
+        /// @see Application::getAbsoluteMousePosition()
+        iPoint getCursorPosition();
+        /// @brief 设置鼠标的绝对位置
+        /// @param pos 要设置的鼠标位置的坐标
+        void setCursorPosition(iPoint pos);
+        /// @brief 从剪贴板获取文本
+        /// @return 剪贴板文本
+        std::wstring getCilpBoardText();
+        /// @brief 向剪贴板设置文本
+        /// @param text 要设置的剪贴板文本
+        void setCilpBoardText(std::wstring text);
+    }
+}
---
+++ src/System.cpp
@@ -0,0 +1,52 @@
+#include "System.h"
+#include "ege.h"
+#include <windows.h>
+
+namespace GFt {
+    namespace Sys {
+        unsigned short getKeyState(Key key) {
+            return GetKeyState(static_cast<int>(key));
+        }
+        unsigned short getAsyncKeyState(Key key) {
+            return GetAsyncKeyState(static_cast<int>(key));
+        }
+        iPoint getCursorPosition() {
+            POINT p;
+            GetCursorPos(&p);
+            return iPoint(p.x, p.y);
+        }
+        void setCursorPosition(iPoint pos) {
+            SetCursorPos(pos.x(), pos.y());
+        }
+        std::wstring getCilpBoardText() {
+            HWND hWnd = ege::getHWnd();
+            ::OpenClipboard(hWnd);
+            HANDLE hClipMemory = ::GetClipboardData(CF_UNICODETEXT);
+            DWORD dwLength = ::GlobalSize(hClipMemory);
+            LPBYTE lpClipMemory = (LPBYTE)::GlobalLock(hClipMemory);
+            auto text = std::wstring((wchar_t*)lpClipMemory, dwLength / 2);
+            ::GlobalUnlock(hClipMemory);
+            ::CloseClipboard();
+            return std::wstring(text);
+        }
+        void setCilpBoardText(std::wstring text_) {
+            auto text = (std::wstring)text_;
+            DWORD dwLength = text.size() + 1;
+            HANDLE hGlobalMemory = ::GlobalAlloc(GHND, dwLength * 2 + 2);
+            LPBYTE lpGlobalMemory = (LPBYTE)::GlobalLock(hGlobalMemory);
+            for (unsigned int i = 0; i < dwLength; i++) {
+                *lpGlobalMemory++ = (text[i] & 0xff);
+                *lpGlobalMemory++ = (text[i] >> 8) & 0xff;
+            }
+            *lpGlobalMemory++ = (L'\0' & 0xff);
+            *lpGlobalMemory = (L'\0' >> 8) & 0xff;
+            ::GlobalUnlock(hGlobalMemory);
+            HWND hWnd = ege::getHWnd();
+            ::OpenClipboard(hWnd);
+            ::EmptyClipboard();
+            ::SetClipboardData(CF_UNICODETEXT, hGlobalMemory);
+            ::CloseClipboard();
+        }
+    }
+}
+
